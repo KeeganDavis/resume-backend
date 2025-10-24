fae73c363ef49b84638b6e07312fb965475beb74
KeeganDavis <keegan.davis96@gmail.com>
2025-10-24 11:16:24 -0500

    style: cleaned up TF vars for fe, be, and bootstrap

--- terraform/frontend/variables.tf
+++ terraform/frontend/variables.tf
@@ -2,19 +2,16 @@
 variable "fe_project_id" {
   description = "Frontend GCP project id"
   type = string
-#   default = ""
 }
 
 variable "fe_region" {
   description = "Frontend project default region"
   type = string
-#   default = ""
 }
 
 variable "my_domain" {
   description = "My public domain. (Ex: example.com)"
   type = string
-  # default = ""
 }
 
 variable "gh_fe_sa" {
